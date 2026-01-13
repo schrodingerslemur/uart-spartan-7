d49a342a29818f18289cf2cbac55f4978560ad69
Brendan Hendrata <bhendrata@cmu.edu>
2026-01-12 20:07:10 -0500

    receiving works

--- SSegDisplayDriver.sv
+++ SSegDisplayDriver.sv
@@ -1,27 +1,40 @@
-module SSegDisplayDriver (
-  input  logic            clk,
-  input  logic            reset,
-  input  logic [6:0] HEX0,
-  input  logic [6:0] HEX1,
-  input  logic [6:0] HEX2,
-  input  logic [6:0] HEX3,
-  input  logic [6:0] HEX4,
-  input  logic [6:0] HEX5,
-  input  logic [6:0] HEX6,
-  input  logic [6:0] HEX7,
-  input  logic [7:0] dpoints,
-  output logic [3:0] D1_AN,
-  output logic [3:0] D2_AN,
-  output logic [7:0] D1_SEG,
-  output logic [7:0] D2_SEG
-  );
+// ----------------- Helper -----------------------
+module EightSevenSegmentDisplays
+  (input  logic [3:0] HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0,
+   input  logic       CLOCK_100, reset,
+   input  logic [7:0] dec_points,
+   input  logic [7:0] blank,
+   output logic [3:0] D2_AN, D1_AN,
+   output logic [7:0] D2_SEG, D1_SEG);
+
+  logic [7:0] segments7, segments6, segments5, segments4,
+              segments3, segments2, segments1, segments0;
+
+   SevenSegmentDisplay ssd(.num7(HEX7),
+                           .num6(HEX6),
+                           .num5(HEX5),
+                           .num4(HEX4),
+                           .num3(HEX3),
+                           .num2(HEX2),
+                           .num1(HEX1),
+                           .num0(HEX0),
+                           .blank,
+                           .dec_points,
+                           .HEX7(segments7),
+                           .HEX6(segments6),
+                           .HEX5(segments5),
+                           .HEX4(segments4),
+                           .HEX3(segments3),
+                           .HEX2(segments2),
+                           .HEX1(segments1),
+                           .HEX0(segments0)
+                          );
 
   logic cycle_cnt_clr;
   logic index_cnt_en;
   logic [1:0] anode_index;
   logic [3:0] anode_index_onecold;
   logic [16:0] cycle_cnt;
-
   /*
    * This counter reset value should be calculated by hand using the frequency
    * of your source clock to achieve the target frequency of 250Hz to 10KHz.
@@ -42,7 +55,7 @@ module SSegDisplayDriver (
   assign index_cnt_en = cycle_cnt_clr;
 
   SSDCounter #(17) cycle_cntr (
-    .clk,
+    .CLOCK_100,
     .reset,
     .clr(cycle_cnt_clr),
     .cnt(cycle_cnt),
@@ -50,7 +63,7 @@ module SSegDisplayDriver (
     );
 
   SSDCounter #(2) index_cntr (
-    .clk,
+    .CLOCK_100,
     .reset,
     .clr(1'b0),
     .cnt(anode_index),
@@ -62,50 +75,32 @@ module SSegDisplayDriver (
     .out(anode_index_onecold)
     );
 
-  /*
-   *  AN3   AN1        AN3   AN1
-   *  HX7   HX5        HX3   HX1
-   *   | AN2 | AN0      | AN2 | AN0
-   *   | HX6 | HX4      | HX2 | HX0
-   * +-|--|--|--|--+  +-|--|--|--|--+
-   * |             |  |             |
-   * |  DISPLAY 1  |  |  DISPLAY 2  |
-   * |             |  |             |
-   * +-------------+  +-------------+
-   *
-   * The layout of the 7-Segment displays on the Boolean board is shown above to
-   * aid understanding of the below block of code. Note that the HEX inputs are
-   * not actually connected to the anodes. This visualization is just meant to
-   * show the relationship between values from the HEX input and the anodes on
-   * the 7-Segment displays.
-   */
-
   always_comb begin
     D1_AN = anode_index_onecold;
     D2_AN = anode_index_onecold;
 
     case (anode_index)
       2'd0: begin
-        D2_SEG = {~dpoints[0], HEX0};
-        D1_SEG = {~dpoints[4], HEX4};
+        D2_SEG = segments0;
+        D1_SEG = segments4;
       end
       2'd1: begin
-        D2_SEG = {~dpoints[1], HEX1};
-        D1_SEG = {~dpoints[5], HEX5};
+        D2_SEG = segments1;
+        D1_SEG = segments5;
       end
       2'd2: begin
-        D2_SEG = {~dpoints[2], HEX2};
-        D1_SEG = {~dpoints[6], HEX6};
+        D2_SEG = segments2;
+        D1_SEG = segments6;
       end
       2'd3: begin
-        D2_SEG = {~dpoints[3], HEX3};
-        D1_SEG = {~dpoints[7], HEX7};
+        D2_SEG = segments3;
+        D1_SEG = segments7;
       end
     endcase
 
   end
 
-endmodule : SSegDisplayDriver
+endmodule : EightSevenSegmentDisplays
 
 module OneColdDecoder #(
   parameter NUM_OUTPUTS = 4
@@ -115,7 +110,7 @@ module OneColdDecoder #(
   );
 
   always_comb begin
-    out = '1;
+    out = {NUM_OUTPUTS{1'b1}};
     out[sel] = 1'b0;
   end
 
@@ -124,14 +119,14 @@ endmodule : OneColdDecoder
 module SSDCounter #(
   parameter WIDTH = 8
   ) (
-  input  logic clk,
+  input  logic CLOCK_100,
   input  logic reset,
   input  logic clr,
   input  logic en,
   output logic [WIDTH-1:0] cnt
   );
 
-  always_ff @(posedge clk, posedge reset) begin
+  always_ff @(posedge CLOCK_100, posedge reset) begin
     if (reset)
       cnt <= '0;
     else if (clr)
@@ -141,3 +136,68 @@ module SSDCounter #(
   end
 
 endmodule : SSDCounter
+
+/*
+ *  This is essentially the SevenSegmentDisplay from HW2. There are
+ *  two differences.  Firstly, it correctly decodes hexadecimal values
+ *  and not just BCD values.  Secondly, the dec_points input allows
+ *  the decimal points on the 8 displays to be illuminated.
+ */
+module SevenSegmentDisplay
+  (input logic [3:0] num7, num6, num5, num4, num3, num2, num1, num0,
+   input logic [7:0] blank,
+   input logic [7:0] dec_points,
+   output logic [7:0] HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0);
+
+   logic [6:0] seg7, seg6, seg5, seg4, seg3, seg2, seg1, seg0;
+
+   HextoSevenSegment digit0(.hex(num0), .segment(seg0));
+   HextoSevenSegment digit1(.hex(num1), .segment(seg1));
+   HextoSevenSegment digit2(.hex(num2), .segment(seg2));
+   HextoSevenSegment digit3(.hex(num3), .segment(seg3));
+   HextoSevenSegment digit4(.hex(num4), .segment(seg4));
+   HextoSevenSegment digit5(.hex(num5), .segment(seg5));
+   HextoSevenSegment digit6(.hex(num6), .segment(seg6));
+   HextoSevenSegment digit7(.hex(num7), .segment(seg7));
+
+   assign HEX0 = (blank[0]) ? '1 : {~dec_points[0], seg0};
+   assign HEX1 = (blank[1]) ? '1 : {~dec_points[1], seg1};
+   assign HEX2 = (blank[2]) ? '1 : {~dec_points[2], seg2};
+   assign HEX3 = (blank[3]) ? '1 : {~dec_points[3], seg3};
+   assign HEX4 = (blank[4]) ? '1 : {~dec_points[4], seg4};
+   assign HEX5 = (blank[5]) ? '1 : {~dec_points[5], seg5};
+   assign HEX6 = (blank[6]) ? '1 : {~dec_points[6], seg6};
+   assign HEX7 = (blank[7]) ? '1 : {~dec_points[7], seg7};
+
+endmodule: SevenSegmentDisplay
+
+/*
+ * Very similar to the BCDtoSevenSegment module from HW2, but
+ * now with hex support!
+ */
+module HextoSevenSegment
+  (input  logic [3:0] hex,
+   output logic [6:0] segment);
+
+  always_comb
+    unique case (hex)
+      4'h0: segment = 7'b1000000;
+      4'h1: segment = 7'b1111001;
+      4'h2: segment = 7'b0100100;
+      4'h3: segment = 7'b0110000;
+      4'h4: segment = 7'b0011001;
+      4'h5: segment = 7'b0010010;
+      4'h6: segment = 7'b0000010;
+      4'h7: segment = 7'b1111000;
+      4'h8: segment = 7'b0000000;
+      4'h9: segment = 7'b0011000;
+      4'hA: segment = 7'b0001000;
+      4'hB: segment = 7'b0000011;
+      4'hC: segment = 7'b1000110;
+      4'hD: segment = 7'b0100001;
+      4'hE: segment = 7'b0000110;
+      4'hF: segment = 7'b0001110;
+    endcase
+
+endmodule : HextoSevenSegment
+
--- top.sv
+++ top.sv
@@ -1,59 +1,55 @@
 module top (
-    // UART interface
-    input  logic ble_uart_rx,
-    input  logic CLOCK_100,
-    output logic ble_uart_tx,
+  input  logic CLOCK_100,
 
-    // LEDs
-    output logic [15:0] LD,
+  // UART signals
+  input  logic UART_rxd,
+  output logic UART_txd,
 
-    // Buttons
-    input  logic [3:0] BTN,
+  // Buttons and switches
+  input  logic [3:0] BTN,
+  input  logic [15:0] SW,
 
-    // Switches
-    input  logic [15:0] SW,
-
-    // Anodes and segments
-    output logic [3:0] D1_AN, D2_AN,
-    output logic [7:0] D1_SEG, D2_SEG
-
+  // Anodes and segments
+  output logic [3:0] D1_AN, D2_AN,
+  output logic [7:0] D1_SEG, D2_SEG
 );
 
-  logic [7:0] rx_data;
   logic en;
+  logic [7:0] rx_data;
 
-  // Instantiate UART module
   uart #(
     .BAUD_RATE   (115_200),
     .CLOCK_FREQ  (100_000_000),
     .DATA_BITS   (8)
   ) uart_inst (
-    .clock      (CLOCK_100),
-    .reset      (BTN[0]),
-    .tx_data    (SW[7:0]),          // Example data to send
-    .tx_send    (BTN[1]),         // Send data on button press
-    .tx_busy    (),                // Unused in this example
-    .rx_data    (rx_data),
-    .rx_valid   (en),
-    .rx         (ble_uart_tx),
-    .tx         (ble_uart_tx)
+    .clock (CLOCK_100),
+    .reset (BTN[0]),
+
+    .tx_data (SW[7:0]),
+    .tx_send (BTN[1]),
+    .tx_busy (),
+
+    .rx_data (rx_data),
+    .rx_valid (en),
+
+    .rx (UART_rxd),
+    .tx (UART_txd)
   );
 
-  // Instantiate SSegDriver
-  logic [7:0] HEX0, HEX1, HEX2, HEX3, HEX4,
+  logic [3:0] HEX0, HEX1, HEX2, HEX3, HEX4,
               HEX5, HEX6, HEX7;
   logic [7:0] received_val;
-  SSegDisplayDriver sseg (
-    .clk(CLOCK_100),
+  EightSevenSegmentDisplays sseg (
+    .CLOCK_100,
     .reset(),
-    .dpoints(8'b0),
+    .dec_points(8'b0),
+    .blank(8'b0),
     .*
   );
 
   // Display values
-  assign HEX7 = received_val;
-  assign HEX6 = rx_data;
-  assign HEX0 = SW[7:0];
+  assign HEX0 = SW[7:0]; // tx value
+  assign HEX7 = received_val; // rx value
 
   // Register
   always_ff @(posedge CLOCK_100 or posedge BTN[0]) begin
